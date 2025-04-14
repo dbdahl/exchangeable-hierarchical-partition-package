5461472d5cd22edc56dea0d0d7b6b8b1b0563b0b
David B. Dahl <dahl@stat.byu.edu>
2025-04-14 17:10:41 -0600

    Rename package

--- gupd/src/shim.c
+++
@@ -1,4 +0,0 @@
-// Automatically regenerated. Do not edit.
-
-void R_init_gupd_rust(void *dll); 
-void R_init_gupd(void *dll) { R_init_gupd_rust(dll); }
---
+++ xhp/src/shim.c
@@ -0,0 +1,4 @@
+// Automatically regenerated. Do not edit.
+
+void R_init_xhp_rust(void *dll); 
+void R_init_xhp(void *dll) { R_init_xhp_rust(dll); }
